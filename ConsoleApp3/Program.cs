c962c0c721582303963f1db36fd3dac8043af7cf
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 16:37:07 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using MathsCalculations;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            MultiSum multisum = new MultiSum();
+            Factorial factorial = new Factorial();
+            Multiplication multiplication = new Multiplication();
+
+            Console.WriteLine("Enter a program number");
+            string program = Console.ReadLine() ?? string.Empty;
+
+            switch (program)
+            {
+                case "1":
+                    string sumN = sum.SumToN();
+                    Console.WriteLine(sumN);
+                    break;
+
+                case "2":
+                    string multisumN = multisum.MultiSumToN();
+                    Console.WriteLine(multisumN);
+                    break;
+
+                case "3":
+                    Console.WriteLine("Please choose 1 for the sum or 2 for the factorial.");
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        string sumNAgain = sum.SumToN();
+                        Console.WriteLine(sumNAgain);
+                    }
+
+                    else if (c == 2)
+                    {
+                        string factorialN = factorial.FactorialToN();
+                        Console.WriteLine(factorialN);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Sorry this is not a correct option!");
+                    }
+                    break;
+
+                case "4":
+                    multiplication.MultiplicationToN();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Not a prorgram");
+                    break;
+            }
+        }
+    }
+}
---
+++ MathsCalculations/Class1.cs
@@ -0,0 +1,83 @@
+﻿namespace MathsCalculations
+{
+    public class Sum
+    {
+
+        public string SumToN()
+        {
+            int i, n, sum = 0;
+            Console.WriteLine("Enter the value of n :");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            return "Total sum: " + sum;
+        }
+    }
+
+    public class MultiSum
+    {
+
+        public string MultiSumToN()
+        {
+            int i, n, a, b, multisum = 0;
+            Console.WriteLine("Enter the value of n :");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                a = i % 3;
+                b = i % 5;
+                if (a == 0 || b == 0)
+                {
+                    multisum += i;
+                }
+
+            }
+
+            return "Total sum: " + multisum;
+        }
+    }
+
+    public class Factorial
+    {
+        public string FactorialToN()
+        {
+            int i, f = 1, n;
+            Console.WriteLine("Enter the value of n :");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+
+            return "The Factorial is: " + f;
+
+        }
+    }
+
+    public class Multiplication
+    {
+        public void MultiplicationToN()
+        {
+            int i, f = 1, n;
+            Console.WriteLine("Enter number to print table: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= 12; i++)
+            {
+                f = n * i;
+                Console.WriteLine("{0} X {1} = {2}", n, i, f);
+            }
+        }
+
+    }
+}
